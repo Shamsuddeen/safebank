961c2aa83ecd20a62af5f90d375b7f779ba75d1b
Shamsuddeen Omacy <omacys2@gmail.com>
2023-05-12 11:00:19 +0100

    User and Auth Controller

---
+++ Controller/user.js
@@ -0,0 +1,25 @@
+const ErrorResponse = require('../Utils/errorResponse');
+const asyncHandler = require('../Middleware/async');
+const User = require('../Model/User');
+
+exports.getUsers = asyncHandler(async (req, res, next) => {
+    const users = await User.find();
+    res.status(200).json({
+        success: true,
+        status: "success",
+        data: users
+    });
+});
+
+exports.getUser = asyncHandler(async (req, res, next) => {
+    const user = await User.findById(req.params.id, (err, result) => {
+        if (!result) {
+            return next(new ErrorResponse("User not foud!", 404));
+        }
+        res.status(200).json({
+            success: true,
+            status: "success",
+            data: result
+        })
+    })
+});
