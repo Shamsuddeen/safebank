e393dfa4779be804aaf3de9ebbfe49d18659e8bd
Shamsuddeen Omacy <omacys2@gmail.com>
2023-05-16 10:04:51 +0100

    Added Virtual Account Issueance

--- Controller/account.js
+++ Controller/account.js
@@ -101,6 +101,49 @@ exports.createAccount = asyncHandler(async (req, res, next) => {
     });
 });
 
+exports.virtualAccount = asyncHandler(async (req, res, next) => {
+    // Check if user exists
+    const user = await User.findById(req.body.user);
+    if (!user._id) {
+        return next(new ErrorResponse("User not found!", 404));
+    }
+    // console.log(user);
+    const name = user.firstName + ' ' + user.lastName
+    // console.log('====================================');
+    // console.log(req);
+    // console.log('====================================');
+    // Create and Map Account to Customer with default funding source
+    const account = await sendRequest('safehaven', '/virtual-accounts', 'post', {
+        validFor: 900,
+        settlementAccount: {
+            bankCode: "999240",
+            accountNumber: process.env.SAFEHAVEN_DEBIT_ACCOUNT
+        },
+        accountName: name,
+        amountControl: "Fixed",
+        callbackUrl: "https://localhost:3000/callback",
+        amount: 1000
+    });
+    console.log(account);
+    if (account.statusCode != 200) {
+        return next(new ErrorResponse("Request Error, Unable to create account", 400));
+    }
+
+    // Save Account to DB
+    const result = await Account.create({
+        user: req.body.user,
+        accountId: account.data._id,
+        number: account.data.accountNumber,
+        name: account.data.accountName
+    });
+
+    res.status(201).json({
+        status: "success",
+        message: 'Account created successfully',
+        data: result
+    });
+});
+
 exports.updateAccount = asyncHandler(async (req, res, next) => {
     const account = await Account.findByIdAndUpdate(req.params.id, req.body, {
         new: true,
